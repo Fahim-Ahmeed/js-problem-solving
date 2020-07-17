1261de19967737dbbd91c38d8633a4cb26b9a8fe
FAHIM AHMED <fahimfahad35@gmail.com>
2020-07-17 17:06:01 +0600

    tinyfriend is solved

--- assignment.js
+++ assignment.js
@@ -1,19 +1,19 @@
-// //problem-1:feetToMile;
-// function feetToMile(feet){
-//     let mile=feet/5280;
-//     return mile;
-// } 
+//problem-1:feetToMile;
+function feetToMile(feet){
+    let mile=feet/5280;
+    return mile;
+} 
 
 
 
-// //problem-2:woodCalculator;
-// function woodCalculator(chair,table,bed){
-//     chair=chair*4;
-//     table=table*8;
-//     bed=table*10;
-//     let totalWood=chair+table+bed;
-//     return totalWood;
-// }
+//problem-2:woodCalculator;
+function woodCalculator(chair,table,bed){
+    chair=chair*4;
+    table=table*8;
+    bed=table*10;
+    let totalWood=chair+table+bed;
+    return totalWood;
+}
 
 
 
